dceeb500eca755c80720135d6cd66e8f7fce5acc
aysenurka <aysenurk.95@outlook.com>
2019-03-27 09:29:09 +0300

    here we go

---
+++ HelpDesk.DAL/MyContext.cs
@@ -0,0 +1,18 @@
+﻿using HelpDesk.Models.IdentityEntities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpDesk.DAL
+{
+    public class MyContext:IdentityDbContext<ApplicationUser,ApplicationRole,string>
+    {
+        public MyContext(DbContextOptions<MyContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
